d9084e4059a712420852fa5c0959abe5d1bd5f60
mstechly <michal.stechly@gmail.com>
2024-05-15 20:03:08 -0700

    remove unnecessary docstrings

--- src/bartiq/integrations/latex.py
+++ src/bartiq/integrations/latex.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Utilities for rendering estimators in LaTeX."""
 from sympy import latex, symbols
 
 from .._routine import Routine
