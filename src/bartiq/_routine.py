fbb33730faebca52266a70e9be1c6a63c6bd6479
bdg221 <brian.goldsmith@gmail.com>
2024-05-30 08:59:56 -0700

    Forgot a character in the repr output

--- src/bartiq/_routine.py
+++ src/bartiq/_routine.py
@@ -442,7 +442,7 @@ class Connection(BaseModel):
     parent: Optional[Routine] = Field(exclude=True, default=None)
 
     def __repr__(self):
-        return f'{self.parent.name}.#{self.source.name} -> {self.parent.name}.{self.target.name}'
+        return f'{self.parent.name}.#{self.source.name} -> {self.parent.name}.#{self.target.name}'
 
     @field_serializer("source", "target")
     def _serialize_port(self, port):
