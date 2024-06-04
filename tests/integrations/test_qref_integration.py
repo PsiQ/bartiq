e01e5579836508955617b602c72da55c208e9170
mstechly <michal.stechly@gmail.com>
2024-06-04 16:27:10 -0400

    test: update items order in qref tests

--- tests/integrations/test_qref_integration.py
+++ tests/integrations/test_qref_integration.py
@@ -75,40 +75,40 @@ def example_serialized_qref_v1_object():
         "version": "v1",
         "program": {
             "name": "root",
-            "type": None,
-            "ports": [
-                {"name": "in_0", "size": "N", "direction": "input"},
-                {"name": "out_0", "size": "N", "direction": "output"},
-                {"name": "out_1", "size": 3, "direction": "output"},
-            ],
-            "resources": [{"name": "n_qubits", "type": "additive", "value": 30}],
-            "input_params": ["N"],
             "children": [
-                {
-                    "name": "foo",
-                    "type": None,
-                    "input_params": ["M"],
-                    "ports": [
-                        {"name": "in_0", "size": "M", "direction": "input"},
-                        {"name": "out_0", "size": 3, "direction": "output"},
-                    ],
-                },
                 {
                     "name": "bar",
                     "type": None,
-                    "input_params": ["N"],
                     "ports": [
-                        {"name": "in_0", "size": "N", "direction": "input"},
-                        {"name": "out_0", "size": "N", "direction": "output"},
+                        {"name": "in_0", "direction": "input", "size": "N"},
+                        {"name": "out_0", "direction": "output", "size": "N"},
                     ],
+                    "input_params": ["N"],
+                },
+                {
+                    "name": "foo",
+                    "type": None,
+                    "ports": [
+                        {"name": "in_0", "direction": "input", "size": "M"},
+                        {"name": "out_0", "direction": "output", "size": 3},
+                    ],
+                    "input_params": ["M"],
                 },
             ],
-            "linked_params": [{"source": "N", "targets": ["foo.M", "bar.N"]}],
-            "connections": [
-                {"source": "in_0", "target": "foo.in_0"},
-                {"source": "foo.out_0", "target": "out_0"},
-                {"source": "bar.out_0", "target": "out_1"},
+            "type": None,
+            "ports": [
+                {"name": "in_0", "direction": "input", "size": "N"},
+                {"name": "out_0", "direction": "output", "size": "N"},
+                {"name": "out_1", "direction": "output", "size": 3},
             ],
+            "resources": [{"name": "n_qubits", "type": "additive", "value": 30}],
+            "connections": [
+                {"source": "bar.out_0", "target": "out_1"},
+                {"source": "foo.out_0", "target": "out_0"},
+                {"source": "in_0", "target": "foo.in_0"},
+            ],
+            "input_params": ["N"],
+            "linked_params": [{"source": "N", "targets": ["foo.M", "bar.N"]}],
         },
     }
 
