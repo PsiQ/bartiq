66f96e66795f3766a0b0f6751e5a908c73b1e2ed
Konrad Jałowiecki <dexter2206@gmail.com>
2024-06-07 18:39:14 +0200

    Improve handling of indirect descendants when passing linked params

--- src/bartiq/compilation/_compile.py
+++ src/bartiq/compilation/_compile.py
@@ -368,14 +368,13 @@ def _pass_on_inherited_params(routine: RoutineWithFunction[T_expr]) -> None:
     for local_ancestor_param, links in routine.linked_params.items():
         global_ancestor_param = join_paths(routine.absolute_path, local_ancestor_param)
         for inheritor_path, param_name in links:
-            try:
-                inheritor = routine.children[inheritor_path]
-            except KeyError:
-                raise ValueError(
+            if "." in inheritor_path:
+                raise BartiqCompilationError(
                     "Error when passing inherited params. "
                     f"The inheritor {inheritor_path} is not a direct descendant of {routine.name}. "
                     "Make sure the parameter linkage happen only one level deep."
                 )
+            inheritor = routine.children[inheritor_path]
             # Define ancestor-to-inheritor parameter map
             param_map = {join_paths(inheritor.absolute_path, param_name): global_ancestor_param}
 
