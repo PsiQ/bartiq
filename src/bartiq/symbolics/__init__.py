8d8c76243b37582d7946355fda3bb6a2fbcf0f6f
Konrad Jałowiecki <dexter2206@gmail.com>
2024-07-15 14:34:19 +0200

    Use new parsing function in SympyBackend

--- src/bartiq/symbolics/__init__.py
+++ src/bartiq/symbolics/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .sympy_backends import sympy_backend
+
+__all__ = ["sympy_backend"]
