af99cee618594fd7b86fa05f373f5faab70d4bdb
Sitong Liu <liusitong@keio.jp>
2024-07-25 13:55:05 -0700

    Rewrite aggregation function for subroutine.

--- src/bartiq/transform.py
+++ src/bartiq/transform.py
@@ -17,6 +17,7 @@ import sympy
 from typing import Union
 from qref import SchemaV1
 
+
 def _expand_aggregation_dict(aggregation_dict: dict) -> dict:
     # Expand the aggregation_dict to handle nested calls
     expanded_dict = {}
@@ -29,7 +30,9 @@ def _expand_aggregation_dict(aggregation_dict: dict) -> dict:
                 sub_mapping = {k: sympy.sympify(v) for k, v in aggregation_dict[current].items()}
                 for sub_key, sub_multiplier in sub_mapping.items():
                     if sub_key in expanded_mapping:
-                        expanded_mapping[sub_key] = sympy.sympify(expanded_mapping[sub_key]) + expanded_mapping[current] * sub_multiplier
+                        expanded_mapping[sub_key] = (
+                            sympy.sympify(expanded_mapping[sub_key]) + expanded_mapping[current] * sub_multiplier
+                        )
                     else:
                         expanded_mapping[sub_key] = expanded_mapping[current] * sub_multiplier
                         to_expand.append(sub_key)
@@ -38,43 +41,30 @@ def _expand_aggregation_dict(aggregation_dict: dict) -> dict:
     return expanded_dict
 
 
-def expand_aggregation_resources(qref_obj: Union[SchemaV1, dict]) -> dict:
-    qref_obj = SchemaV1.model_validate(qref_obj)
-
-    for program in qref_obj['programs']:
-        if 'resources' not in program:
-            continue
-        program['resources'] = _expand_aggregation_dict(program['resources'])
-
-    return qref_obj
-
-def add_aggregated_resources(aggregation_dict: dict, qref_obj: Union[SchemaV1, dict]) -> dict:
-    qref_obj = expand_aggregation_resources(qref_obj)
+def add_aggregated_resources(aggregation_dict: dict, subroutine: dict) -> dict:
     expanded_aggregation_dict = _expand_aggregation_dict(aggregation_dict)
+    if "resources" not in subroutine:
+        raise ValueError("No resources defined")
+    # Initialize a dictionary for aggregated resources
+    aggregated_resources = {res["name"]: copy.deepcopy(res) for res in subroutine["resources"]}
 
-    for program in qref_obj['programs']:
-        if 'resources' not in program:
-            continue
-        # Initialize a dictionary for aggregated resources
-        aggregated_resources = {res["name"]: copy.deepcopy(res) for res in program["resources"]}
+    # Iterate over the original resources in the program
+    for resource in copy.deepcopy(subroutine["resources"]):
+        resource_name = resource["name"]
+        resource_expr = sympy.sympify(resource["value"])
 
-        # Iterate over the original resources in the program
-        for resource in copy.deepcopy(program["resources"]):
-            resource_name = resource["name"]
-            resource_expr = sympy.sympify(resource["value"])
+        # Check if the resource should be aggregated
+        if resource_name in expanded_aggregation_dict:
+            mapping = expanded_aggregation_dict[resource_name]
+            for key, multiplier in mapping.items():
+                if key in aggregated_resources:
+                    aggregated_resources[key]["value"] = (
+                        sympy.sympify(aggregated_resources[key]["value"]) + multiplier * resource_expr
+                    )
+                else:
+                    aggregated_resources[key] = {"name": key, "type": "additive", "value": multiplier * resource_expr}
+            del aggregated_resources[resource_name]
 
-            # Check if the resource should be aggregated
-            if resource_name in expanded_aggregation_dict:
-                mapping = expanded_aggregation_dict[resource_name]
-                for key, multiplier in mapping.items():
-                    if key in aggregated_resources:
-                        aggregated_resources[key]["value"] = (
-                            sympy.sympify(aggregated_resources[key]["value"]) + multiplier * resource_expr
-                        )
-                    else:
-                        aggregated_resources[key] = {"name": key, "type": "additive", "value": multiplier * resource_expr}
-                del aggregated_resources[resource_name]
+    subroutine["resources"] = list(aggregated_resources.values())
 
-        program["resources"] = list(aggregated_resources.values())
-
-    return qref_obj
+    return subroutine
