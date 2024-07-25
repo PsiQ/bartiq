3b490f23ef4d269079e8d46f48ba362621e23fbc
Sitong Liu <liusitong@keio.jp>
2024-07-25 15:42:51 -0700

    Rewrite aggregation function for subroutine.

--- src/bartiq/transform.py
+++ src/bartiq/transform.py
@@ -13,35 +13,54 @@
 # limitations under the License.
 
 import copy
+from typing import Any, Dict
+
 import sympy
-from typing import Union
-from qref import SchemaV1
 
 
-def _expand_aggregation_dict(aggregation_dict: dict) -> dict:
-    # Expand the aggregation_dict to handle nested calls
+def _expand_aggregation_dict(aggregation_dict: Dict[str, Dict[str, Any]]) -> Dict[str, Dict[str, Any]]:
+    """
+    Expand the aggregation dictionary to handle nested resources.
+
+    Args:
+        aggregation_dict (Dict[str, Dict[str, Any]]): The input aggregation dictionary.
+
+    Returns:
+        Dict[str, Dict[str, Any]]: The expanded aggregation dictionary.
+    """
     expanded_dict = {}
-    for key, mapping in aggregation_dict.items():
+    for resource, mapping in aggregation_dict.items():
         expanded_mapping = {k: sympy.sympify(v) for k, v in mapping.items()}
         to_expand = list(mapping.keys())
         while to_expand:
             current = to_expand.pop(0)
             if current in aggregation_dict:
                 sub_mapping = {k: sympy.sympify(v) for k, v in aggregation_dict[current].items()}
-                for sub_key, sub_multiplier in sub_mapping.items():
-                    if sub_key in expanded_mapping:
-                        expanded_mapping[sub_key] = (
-                            sympy.sympify(expanded_mapping[sub_key]) + expanded_mapping[current] * sub_multiplier
+                for sub_res, sub_multiplier in sub_mapping.items():
+                    if sub_res in expanded_mapping:
+                        expanded_mapping[sub_res] = (
+                            sympy.sympify(expanded_mapping[sub_res]) + expanded_mapping[current] * sub_multiplier
                         )
                     else:
-                        expanded_mapping[sub_key] = expanded_mapping[current] * sub_multiplier
-                        to_expand.append(sub_key)
+                        expanded_mapping[sub_res] = expanded_mapping[current] * sub_multiplier
+                        to_expand.append(sub_res)
                 del expanded_mapping[current]
-        expanded_dict[key] = expanded_mapping
+        expanded_dict[resource] = expanded_mapping
     return expanded_dict
 
 
-def add_aggregated_resources(aggregation_dict: dict, subroutine: dict) -> dict:
+def add_aggregated_resources(aggregation_dict: Dict[str, Dict[str, Any]], subroutine: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Add aggregated resources to the subroutine based on the aggregation dictionary.
+
+    Args:
+        aggregation_dict (Dict[str, Dict[str, Any]]): The aggregation dictionary.
+        subroutine (Dict[str, Any]): The subroutine to which the resources will be added.
+
+    Returns:
+        Dict[str, Any]: The subroutine with aggregated resources.
+
+    """
     expanded_aggregation_dict = _expand_aggregation_dict(aggregation_dict)
     if "resources" not in subroutine:
         raise ValueError("No resources defined")
@@ -53,16 +72,19 @@ def add_aggregated_resources(aggregation_dict: dict, subroutine: dict) -> dict:
         resource_name = resource["name"]
         resource_expr = sympy.sympify(resource["value"])
 
-        # Check if the resource should be aggregated
         if resource_name in expanded_aggregation_dict:
             mapping = expanded_aggregation_dict[resource_name]
-            for key, multiplier in mapping.items():
-                if key in aggregated_resources:
-                    aggregated_resources[key]["value"] = (
-                        sympy.sympify(aggregated_resources[key]["value"]) + multiplier * resource_expr
+            for sub_res, multiplier in mapping.items():
+                if sub_res in aggregated_resources:
+                    aggregated_resources[sub_res]["value"] = (
+                        sympy.sympify(aggregated_resources[sub_res]["value"]) + multiplier * resource_expr
                     )
                 else:
-                    aggregated_resources[key] = {"name": key, "type": "additive", "value": multiplier * resource_expr}
+                    aggregated_resources[sub_res] = {
+                        "name": sub_res,
+                        "type": "additive",
+                        "value": multiplier * resource_expr,
+                    }
             del aggregated_resources[resource_name]
 
     subroutine["resources"] = list(aggregated_resources.values())
