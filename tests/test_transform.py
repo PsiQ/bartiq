44f66b994bc2b10aa8d5ecfd7877ba3c46c191eb
Sitong Liu <liusitong@keio.jp>
2024-07-30 21:51:17 -0700

    Add tests for aggregation function.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import pytest
+import sympy
 from bartiq import Routine
 from bartiq.integrations import qref_to_bartiq
 from bartiq.transform import add_aggregated_resources
@@ -32,39 +33,53 @@ ccry_gate = {
     "local_variables": {"n": "x"},
 }
 
+arbitrary_z = {
+    "name": "arbitrary_z",
+    "type": None,
+    "ports": [
+        {"name": "in", "direction": "input", "size": "n"},
+        {"name": "out", "direction": "output", "size": "n"},
+    ],
+    "resources": [
+        {"name": "arbitrary_z", "type": "additive", "value": "ceil(5*num/2)"},
+    ],
+    "input_params": ["x", "num"],
+    "local_variables": {"n": "x"},
+}
 
-def generate_test_1():
-    test_1_qref = {
-        "name": "test_1_qref",
+
+def generate_test(subroutine, input_params, linked_params):
+    test_qref = {
+        "name": "test_qref",
         "type": None,
         "ports": [
             {"name": "in", "direction": "input", "size": "z"},
             {"name": "out", "direction": "output", "size": "z"},
         ],
     }
-    test_1_qref["children"] = [ccry_gate]
-    test_1_qref["connections"] = [
-        {"source": "in", "target": "ccry_gate.in"},
-        {"source": "ccry_gate.out", "target": "out"},
+    test_qref["children"] = [subroutine]
+    test_qref["connections"] = [
+        {"source": "in", "target": f"{subroutine['name']}.in"},
+        {"source": f"{subroutine['name']}.out", "target": "out"},
     ]
-    test_1_qref["input_params"] = ["z", "num"]
-    test_1_qref["linked_params"] = [
-        {"source": "z", "targets": ["ccry_gate.x"]},
-        {"source": "num", "targets": ["ccry_gate.num"]},
-    ]
-    test_1_qref = {"version": "v1", "program": test_1_qref}
-    test_1 = qref_to_bartiq(test_1_qref)
-    return test_1
+    test_qref["input_params"] = input_params
+    test_qref["linked_params"] = linked_params
+    test_qref = {"version": "v1", "program": test_qref}
+    return qref_to_bartiq(test_qref)
 
 
 @pytest.mark.parametrize(
-    "aggregation_dict, test_1, expected_output",
+    "aggregation_dict, generate_test_fn, expected_output",
     [
         (
             {"control_ry": {"rotation": 2, "CNOT": 2}, "rotation": {"T_gates": 50}},
-            generate_test_1(),
+            generate_test(
+                ccry_gate,
+                ["z", "num"],
+                [{"source": "z", "targets": ["ccry_gate.x"]}, {"source": "num", "targets": ["ccry_gate.num"]}],
+            ),
             Routine(
-                name="test_1_qref",
+                name="test_qref",
                 input_params=["z", "num"],
                 children={
                     "ccry_gate": Routine(
@@ -94,16 +109,62 @@ def generate_test_1():
                 ],
             ),
         ),
+        (  # Example using aggregation dict values with parameters to approximate single-qubit Z-rotations
+            # using optimal ancilla-free Clifford+T circuits, as detailed in (arXiv:1403.2975).
+            {"arbitrary_z": {"T_gates": "3*log_2(1/epsilon) + O(log(log(1/epsilon)))"}},
+            generate_test(
+                arbitrary_z,
+                ["z", "num"],
+                [{"source": "z", "targets": ["arbitrary_z.x"]}, {"source": "num", "targets": ["arbitrary_z.num"]}],
+            ),
+            Routine(
+                name="test_qref",
+                input_params=["z", "num"],
+                children={
+                    "arbitrary_z": Routine(
+                        name="arbitrary_z",
+                        type=None,
+                        input_params=["x", "num"],
+                        ports={
+                            "in": {"name": "in", "direction": "input", "size": "n"},
+                            "out": {"name": "out", "direction": "output", "size": "n"},
+                        },
+                        resources={
+                            "T_gates": {
+                                "name": "T_gates",
+                                "type": "additive",
+                                "value": "ceiling(5*num/2)*(3*log_2(1/epsilon) + O(log(log(1/epsilon))))",
+                            },
+                        },
+                        local_variables={"n": "x"},
+                    )
+                },
+                type=None,
+                linked_params={"z": [("arbitrary_z", "x")], "num": [("arbitrary_z", "num")]},
+                ports={
+                    "in": {"name": "in", "direction": "input", "size": "z"},
+                    "out": {"name": "out", "direction": "output", "size": "z"},
+                },
+                connections=[
+                    {"source": "in", "target": "arbitrary_z.in"},
+                    {"source": "arbitrary_z.out", "target": "out"},
+                ],
+            ),
+        ),
     ],
 )
-def test_add_aggregated_resources(aggregation_dict, test_1, expected_output):
-    result = add_aggregated_resources(aggregation_dict, test_1)
+def test_add_aggregated_resources(aggregation_dict, generate_test_fn, expected_output):
+    result = add_aggregated_resources(aggregation_dict, generate_test_fn)
+    compare_routines(result, expected_output)
 
-    def compare_resources(routine, expected):
-        if hasattr(routine, "resources") and routine.resources:
-            for resource_name in routine.resources:
-                assert routine.resources[resource_name].value == expected.resources[resource_name].value
-            for child in routine.children:
-                compare_resources(child, expected)
 
-    compare_resources(result, expected_output)
+# Define the comparison function
+def compare_routines(routine, expected):
+    if hasattr(routine, "resources") and routine.resources:
+        for resource_name in routine.resources:
+            expanded_expr = sympy.simplify(routine.resources[resource_name].value)
+            expected_expr = sympy.simplify(expected.resources[resource_name].value)
+            assert expanded_expr.equals(expected_expr)
+
+    for child in routine.children:
+        compare_routines(routine.children[child], expected.children[child])
