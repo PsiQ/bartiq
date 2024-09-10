e9c206e08cab49bbd63e7fb62d3be5519219bee7
Konrad Jałowiecki <dexter2206@gmail.com>
2024-09-10 16:07:25 +0200

    Remove superfluous utility functions

--- src/bartiq/compilation/_utilities.py
+++ src/bartiq/compilation/_utilities.py
@@ -13,13 +13,7 @@
 # limitations under the License.
 
 import ast
-from typing import Any, TypeVar
-
-from .. import Routine
-from ..errors import BartiqCompilationError
-from .types import NUMBER_TYPES, Number
-
-T = TypeVar("T", bound=Routine)
+from typing import Any
 
 
 def is_single_parameter(expression: Any) -> bool:
@@ -54,63 +48,3 @@ def _is_python_tree_a_single_param(tree):
         # And this expression contains only a variable Name or an Attribute access?
         isinstance(tree.body[0].value, (ast.Name, ast.Attribute))
     )
-
-
-def is_number_string(s: str) -> bool:
-    """Returns False if the string encodes a number."""
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
-def is_non_negative_int(value: Any) -> bool:
-    """Returns True if the expression corresponds to a positive integer."""
-    try:
-        return float(value) >= 0 and int(value) == float(value)
-    except ValueError:
-        return False
-
-
-def is_constant_int(expression: Any) -> bool:
-    """Returns True if the expression corresponds to a constant."""
-    expression = str(expression)
-    if isinstance(expression, str):
-        try:
-            int(expression)
-            return True
-        except ValueError:
-            return False
-    return isinstance(expression, int)
-
-
-def get_children_in_walk_order(routine: T) -> list[T]:
-    """Returns routine children in walk order."""
-    return [op for op in routine.walk() if op in routine.children.values()]
-
-
-def parse_value(value_str: str) -> Number:
-    """Attempts to parse a single value string, but throws a compilation error if this isn't possible."""
-    try:
-        value = ast.literal_eval(value_str)
-        if not isinstance(value, NUMBER_TYPES):
-            raise ValueError
-        return value
-    except ValueError:
-        raise BartiqCompilationError(f"Could not parse value {value_str}; values must be integers or floats.")
-
-
-def split_equation(equation: str) -> tuple[str, str]:
-    """Splits an equation string and returns the left and right side."""
-    if equation.count("=") != 1:
-        raise ValueError(f"Equations must contain a single equals sign; found {equation}")
-
-    lhs, rhs = equation.split("=")
-    lhs = lhs.strip()
-    rhs = rhs.strip()
-
-    if not lhs or not rhs:
-        raise ValueError(f"Equations must have both a left- and right-hand side; found {equation}")
-
-    return (lhs, rhs)
