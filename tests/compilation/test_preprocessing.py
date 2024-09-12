7a1d3ab9f33a240f36231f715664e04306d1110a
Konrad Jałowiecki <dexter2206@gmail.com>
2024-09-12 13:20:26 +0200

    Convert propagate_linked_param test cases to qref

--- tests/compilation/test_preprocessing.py
+++ tests/compilation/test_preprocessing.py
@@ -1,9 +1,11 @@
+import pytest
 from qref.schema_v1 import RoutineV1
 
 from bartiq._routine import Routine, routine_to_qref
 from bartiq.compilation.preprocessing import (
     PreprocessingStage,
     add_default_additive_resources,
+    propagate_linked_params,
 )
 
 
@@ -50,3 +52,122 @@ def test_adding_additive_resources(backend):
     expected_routine = RoutineV1.model_validate({**routine.model_dump(), "resources": expected_resources})
 
     assert routine_with_resources == expected_routine
+
+
+LINKED_PARAM_CASES = [
+    (
+        {
+            "name": "root",
+            "type": None,
+            "children": [
+                {
+                    "name": "a",
+                    "type": None,
+                    "children": [
+                        {
+                            "name": "b",
+                            "type": None,
+                            "resources": [{"name": "z", "type": "other", "value": "x+y"}],
+                            "input_params": ["x", "y"],
+                        },
+                    ],
+                    "resources": [{"name": "v", "type": "other", "value": "w+b.z"}],
+                    "input_params": ["w"],
+                },
+            ],
+            "resources": [{"name": "u", "type": "other", "value": "a.v+5"}],
+            "input_params": ["w_a", "x_ab", "y_ab"],
+            "linked_params": [
+                {"source": "w_a", "targets": ["a.w"]},
+                {"source": "x_ab", "targets": ["a.b.x"]},
+                {"source": "y_ab", "targets": ["a.b.y"]},
+            ],
+        },
+        {
+            "root": {
+                "w_a": (("a", "w"),),
+                "x_ab": (("a", "b.x"),),
+                "y_ab": (("a", "b.y"),),
+            },
+            "root.a": {
+                "b.x": (("b", "x"),),
+                "b.y": (("b", "y"),),
+            },
+        },
+    ),
+    (
+        {
+            "name": "root",
+            "type": None,
+            "children": [
+                {
+                    "name": "a",
+                    "type": None,
+                    "children": [
+                        {
+                            "name": "b",
+                            "type": None,
+                            "children": [
+                                {
+                                    "name": "c",
+                                    "type": None,
+                                    "resources": [{"name": "z", "type": "other", "value": "x+y"}],
+                                    "input_params": ["x", "y"],
+                                }
+                            ],
+                            "resources": [{"name": "z", "type": "other", "value": "c.z+y"}],
+                            "input_params": ["y"],
+                        },
+                        {
+                            "name": "d",
+                            "type": None,
+                            "resources": [{"name": "z", "type": "other", "value": "x+y"}],
+                            "input_params": ["x", "y"],
+                        },
+                    ],
+                    "resources": [{"name": "z", "type": "other", "value": "b.z + d.z"}],
+                },
+            ],
+            "resources": [{"name": "z", "type": "other", "value": "a.z+5"}],
+            "input_params": ["x_abc", "y_abc", "y_ab", "x_ad", "y_ad"],
+            "linked_params": [
+                {"source": "x_abc", "targets": ["a.b.c.x"]},
+                {"source": "y_abc", "targets": ["a.b.c.y"]},
+                {"source": "y_ab", "targets": ["a.b.y"]},
+                {"source": "x_ad", "targets": ["a.d.x"]},
+                {"source": "y_ad", "targets": ["a.d.y"]},
+            ],
+        },
+        {
+            "root": {
+                "x_abc": (("a", "b.c.x"),),
+                "x_ad": (("a", "d.x"),),
+                "y_ab": (("a", "b.y"),),
+                "y_abc": (("a", "b.c.y"),),
+                "y_ad": (("a", "d.y"),),
+            },
+            "root.a": {
+                "b.c.x": (("b", "c.x"),),
+                "d.x": (("d", "x"),),
+                "b.y": (("b", "y"),),
+                "b.c.y": (("b", "c.y"),),
+                "d.y": (("d", "y"),),
+            },
+            "root.a.b": {"c.x": (("c", "x"),), "c.y": (("c", "y"),)},
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize("input_dict, expected_linked_params", LINKED_PARAM_CASES)
+def test_precompile_propagates_linked_params(input_dict, expected_linked_params, backend):
+    input_routine = Routine.from_qref(RoutineV1(**input_dict), backend)
+
+    preprocessed_routine = propagate_linked_params(input_routine, backend)
+
+    for path, linked_params in expected_linked_params.items():
+        routine = preprocessed_routine
+        for part in path.split(".")[1:]:
+            routine = routine.children[part]
+
+        assert routine.linked_params == linked_params
