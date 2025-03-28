b8b60a971664e5d99689a82e6f5486360746a9c6
Brendan Reid <breid@psiquantum.com>
2025-03-28 10:52:33 +0000

    bug fix

--- src/bartiq/symbolics/symbolic_manipulator.py
+++ src/bartiq/symbolics/symbolic_manipulator.py
@@ -431,32 +431,32 @@ def _parse_assumption(assumption: str) -> tuple[str, str, int | float, dict[str,
     var, relationship, value = _unpack_assumption(assumption=assumption)
     properties: dict[str, bool] = {}
     try:
-        gt: bool = relationship == _Relationships.GREATER_THAN
-        gte: bool = relationship == _Relationships.GREATER_THAN_OR_EQUAL_TO
-
-        lt: bool = relationship == _Relationships.LESS_THAN
-        lte: bool = relationship == _Relationships.LESS_THAN_OR_EQUAL_TO
-
-        value_positive: bool = value >= 0
-        value_negative: bool = value <= 0
-        value_non_zero: bool = value != 0
-
-        reference_value = eval(value)
-        properties.update(
-            dict(
-                positive=((gt or gte) and value_positive) or None,
-                negative=((lt or lte) and value_negative) or None,
-                nonzero=((gt and value_positive and value_non_zero) or (lt and value_negative and value_non_zero))
-                or None,
-            )
-        )
-
-        return (var, relationship, reference_value, properties)
+        value = eval(value)
     except NameError:
         raise NotImplementedError(
             f"""Assumption tries to draw a relationship between two variables: {var}, {value}.
             At present, this is not possible!"""
         )
+
+    gt: bool = relationship == _Relationships.GREATER_THAN
+    gte: bool = relationship == _Relationships.GREATER_THAN_OR_EQUAL_TO
+
+    lt: bool = relationship == _Relationships.LESS_THAN
+    lte: bool = relationship == _Relationships.LESS_THAN_OR_EQUAL_TO
+
+    value_positive: bool = value >= 0
+    value_negative: bool = value <= 0
+    value_non_zero: bool = value != 0
+
+    properties.update(
+        dict(
+            positive=((gt or gte) and value_positive) or None,
+            negative=((lt or lte) and value_negative) or None,
+            nonzero=((gt and value_positive and value_non_zero) or (lt and value_negative and value_non_zero)) or None,
+        )
+    )
+
+    return (var, relationship, value, properties)
 
 
 def _unpack_assumption(assumption: str) -> tuple[str, str, str]:
