8f5459ce0f8df7893677f683f7e18b89ad1f8fca
Konrad Jałowiecki <dexter2206@gmail.com>
2025-04-07 13:45:00 +0200

    feat: Add backend sum and backend prod (#182)
    * Add sum method to backend interface
    
    * Add test for catching perf regression in preprocessing
    
    * Add prod method to backend interface
    
    * Add test case for prod
    
    * Use sum and prod in preprocessing
    
    * Use backend.sum when computing highwater
    
    * Add pytest-timeout do dev dependencies
    
    * Add timeout to regression perf test for preprocessing resources

--- src/bartiq/compilation/preprocessing.py
+++ src/bartiq/compilation/preprocessing.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
 from collections import defaultdict
 from dataclasses import replace
 from typing import Callable
