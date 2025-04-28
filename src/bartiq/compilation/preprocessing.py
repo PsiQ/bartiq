a9aa99c09ea627ec817fbfcda2ed9c2af03ffc8c
Michał Stęchły <michal.stechly@gmail.com>
2025-04-28 17:55:12 +0200

    fix: move adding arithmetic resources from preprocessing to compilation (#190)
    * fix: move adding arithmetic resources from preprocessing to compilation
    
    * docs: update docstrings
    
    * test: increase the timeout for a long test

--- src/bartiq/compilation/preprocessing.py
+++ src/bartiq/compilation/preprocessing.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
 from collections import defaultdict
 from dataclasses import replace
 from typing import Callable
--- tests/compilation/test_derived_resources.py
+++ tests/compilation/test_derived_resources.py
@@ -119,9 +119,7 @@ def test_highwater_of_an_empty_routine_is_zero():
     assert compiled_routine.resources["qubit_highwater"].value == 0
 
 
-@pytest.mark.parametrize("transitive_resources", [True, False])
-def test_additive_derived_resources_are_processed_correctly(transitive_resources):
-    resource_name = "test_resource"
+def test_additive_derived_resources_are_processed_correctly():
     input_routine = {
         "version": "v1",
         "program": {
@@ -140,27 +138,20 @@ def test_additive_derived_resources_are_processed_correctly(transitive_resources
                     "name": "c",
                     "type": "c",
                     "resources": [
-                        {"name": resource_name, "type": "additive", "value": 7},
+                        {"name": "test_resource", "type": "additive", "value": 7},
                     ],
                 },
             ],
         },
     }
 
-    def add_test_resource(routine, _):
-        if resource_name in routine.resources or len(routine.children) != 0:
+    def add_test_resource(routine, backend):
+        if "test_resource" in routine.resources or len(routine.children) != 0:
             return None
         else:
             return 5
 
-    derived_resources = [{"name": resource_name, "type": "additive", "calculate": add_test_resource}]
-    compilation_result = compile_routine(
-        input_routine, derived_resources=derived_resources, allow_transitive_resources=transitive_resources
-    )
-    compiled_routine = compilation_result.routine
-    backend = compilation_result._backend
-    assert (
-        compiled_routine.resources[resource_name].value == 17
-        if not transitive_resources
-        else backend.as_expression("+".join(f"{child}.{resource_name}" for child in compiled_routine.children))
-    )
+    derived_resources = [{"name": "test_resource", "type": "additive", "calculate": add_test_resource}]
+    compiled_routine = compile_routine(input_routine, derived_resources=derived_resources).routine
+
+    assert compiled_routine.resources["test_resource"].value == 17
