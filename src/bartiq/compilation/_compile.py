b7271b771bd6a057e518f0b0cb1d3d9b141227e0
brendan reid <brendan.reid1991@gmail.com>
2025-05-09 16:39:46 +0100

    refactor: Changed assertions into Exceptions

--- src/bartiq/compilation/_compile.py
+++ src/bartiq/compilation/_compile.py
@@ -242,23 +242,20 @@ def _process_repeated_resources(
 
     # Ensure that routine with repetition only contains resources that we will later overwrite
     child_resources = copy.copy(children[0].resources)
-    # if child_resources_not_in_parent := child_resources.keys() - resources.keys():
-    #     raise BartiqCompilationError(
-    #         """Routine with repetition does not share the same resources as its child."""
-    #         f"""\nFollowing resources are in the child, but not the parent: {child_resources_not_in_parent}"""
-    #     )
-    # if incorrectly_named_resources := [
-    #     x
-    #     for resource in resources.values()
-    #     if (x := backend.serialize(resource.value)) != f"{children[0].name}.{resource.name}"
-    # ]:
-    #     raise BartiqCompilationError(
-    #         """Routine with repetition should have resource names like `child_name.resource_name."""
-    #         f"""\nFound the following incorrectly named resources {incorrectly_named_resources}"""
-    #     )
-    for resource_name, resource in resources.items():
-        assert resource_name in child_resources
-        assert backend.serialize(resource.value) == f"{children[0].name}.{resource.name}"
+    if parent_resources_not_in_child := resources.keys() - child_resources.keys():
+        raise BartiqCompilationError(
+            """Routine with repetition does not share the same resources as its child."""
+            f"""\nFollowing resources are in the parent, but not the child: {parent_resources_not_in_child}"""
+        )
+    if incorrectly_named_resources := [
+        x
+        for resource in resources.values()
+        if (x := backend.serialize(resource.value)) != f"{children[0].name}.{resource.name}"
+    ]:
+        raise BartiqCompilationError(
+            """Routine with repetition should have resource names like `child_name.resource_name."""
+            f"""\nFound the following incorrectly named resources {incorrectly_named_resources}"""
+        )
 
     new_resources = {}
     for resource in child_resources.values():
