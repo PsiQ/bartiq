c62ec312df234aa3a5a92af2707ced9fe56e2113
brendan reid <brendan.reid1991@gmail.com>
2025-05-27 13:41:17 +0100

    feat: Added basic tests for rewriters

---
+++ tests/analysis/_rewriters/basic_rewriter_tests.py
@@ -0,0 +1,54 @@
+import pytest
+from typing import Type
+from bartiq.analysis._rewriters.sympy_rewriter import ExpressionRewriter
+from bartiq.symbolics.backend import SymbolicBackend
+
+
+class ExpressionRewriterTests:
+    rewriter: Type[ExpressionRewriter]
+    backend = Type[SymbolicBackend]
+
+    @pytest.fixture(scope="class")
+    def trivial(self):
+        return self.rewriter("a")
+
+    @pytest.fixture(scope="class")
+    def sum_and_mul(self):
+        return self.rewriter("a + b + c + d + c*d + a*b")
+
+    @pytest.fixture(scope="class")
+    def many_funcs(self):
+        return self.rewriter("a*log2(x/n) + b*(max(1+y, 2+x) + Heaviside(aleph, beth))")
+
+    @pytest.fixture(scope="class")
+    def nested_max(self):
+        return self.rewriter("max(a, 1 - max(b, 1 - max(c, lamda)))")
+
+    @pytest.mark.parametrize(
+        "expression, individual_terms",
+        [
+            ["trivial", ["a"]],
+            ["sum_and_mul", ["a", "b", "c", "d", "c*d", "a*b"]],
+            [
+                "many_funcs",
+                ["a*log2(x/n)", "b*(max(1+y, 2+x) + Heaviside(aleph, beth))"],
+            ],
+        ],
+    )
+    def test_as_individual_terms(self, expression, individual_terms, request):
+        assert set(request.getfixturevalue(expression).as_individual_terms) == set(
+            map(self.backend.as_expression, individual_terms)
+        )
+
+    @pytest.mark.parametrize("fixture", ["trivial", "sum_and_mul", "many_funcs", "nested_max"])
+    def test_variables(self, fixture, request):
+        rewriter = request.getfixturevalue(fixture)
+        assert rewriter.variables == rewriter.expression.free_symbols
+
+    def test_expand(self):
+        expr = self.backend.as_expression("(a + b)*c + d*(log2(x) + 5)")
+        assert self.rewriter(expr).expand() == expr.expand()
+
+    @pytest.mark.parametrize("focus_on, expected_expression", [["a", "a*(b+1)"], ["c", "c*(d+1)"]])
+    def test_focus(self, focus_on, expected_expression, sum_and_mul):
+        assert sum_and_mul.focus(focus_on) == self.backend.as_expression(expected_expression)
---
+++ tests/analysis/_rewriters/test_sympy_rewriter.py
@@ -0,0 +1,63 @@
+import pytest
+import sympy
+
+from bartiq.analysis._rewriters.sympy_rewriter import SympyExpressionRewriter
+from bartiq import sympy_backend
+
+from tests.analysis._rewriters.basic_rewriter_tests import ExpressionRewriterTests
+
+
+class TestSympyExpressionRewriter(ExpressionRewriterTests):
+    rewriter = SympyExpressionRewriter
+    backend = sympy_backend
+
+    def test_simplify(self):
+        expr = self.backend.as_expression("(a*a + b*a)*c + d*(log2(x)**2 + log2(x))")
+        assert self.rewriter(expr).simplify() == expr.simplify()
+
+    @pytest.mark.parametrize(
+        "expression, symbol_names",
+        [
+            ["trivial", ["a"]],
+            ["sum_and_mul", ["a", "b", "c", "d"]],
+            [
+                "many_funcs",
+                ["a", "x", "n", "b", "y", "aleph", "beth"],
+            ],
+        ],
+    )
+    def test_get_symbol(self, expression, symbol_names, request):
+        for name in symbol_names:
+            assert request.getfixturevalue(expression).get_symbol(name) == sympy.Symbol(name)
+
+    def test_get_symbol_raises_error_if_no_symbol_exists(self, many_funcs):
+        sym = "foo"
+        with pytest.raises(ValueError, match=f"No variable '{sym}'."):
+            many_funcs.get_symbol(sym)
+
+    @pytest.mark.parametrize(
+        "fixture, args_and_fns",
+        [
+            ["trivial", []],
+            ["many_funcs", ["log2(x/n)", "max(1+y, 2+x)", "Heaviside(aleph, beth)"]],
+            ["nested_max", ["max(c, lamda)", "max(b, 1-max(c, lamda))", "max(a, 1-max(b, 1-max(c, lamda)))"]],
+        ],
+    )
+    def test_all_functions_and_arguments(self, fixture, args_and_fns, request):
+        request.getfixturevalue(fixture).all_functions_and_arguments() == set(
+            map(self.backend.as_expression, args_and_fns)
+        )
+
+    @pytest.mark.parametrize(
+        "function, expected_args", [["log2", ["x/n"]], ["max", [("x+2", "y+1")]], ["Heaviside", [("aleph", "beth")]]]
+    )
+    def test_list_arguments_of_function(self, function, expected_args, many_funcs):
+
+        assert set(many_funcs.list_arguments_of_function(function)) == set(
+            (
+                tuple(self.backend.as_expression(x) for x in ex)
+                if isinstance(ex, tuple)
+                else self.backend.as_expression(ex)
+            )
+            for ex in expected_args
+        )
