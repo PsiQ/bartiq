176b25d917aec024f6e0119a6d1b2a864f299825
brendan reid <brendan.reid1991@gmail.com>
2025-05-27 13:41:31 +0100

    chore: improved typing, added docstrings

--- src/bartiq/analysis/_rewriters/_expression_rewriter.py
+++ src/bartiq/analysis/_rewriters/_expression_rewriter.py
@@ -1,22 +1,19 @@
 from __future__ import annotations
-from typing import TypeVar, TypeAlias, ParamSpec, Any, Generic
+from typing import TypeAlias, ParamSpec, Any, Generic
 from collections.abc import Callable, Iterable, Mapping
 from bartiq import CompiledRoutine
 from bartiq.symbolics.backend import SymbolicBackend, T
 from abc import abstractmethod, ABC
 
 
-Expression: TypeAlias = T | str
-Symbol: TypeAlias = T | str
-
-
+Expr: TypeAlias = T | str
 P = ParamSpec("P")
 
 
-def update_expression(function: Callable[P, Expression[T]]) -> Callable[P, Expression[T]]:
-    """Decorator for updating the stored expression in Manipulator."""
+def update_expression(function: Callable[P, str | Expr[T]]) -> Callable[P, Expr[T]]:
+    """Decorator for updating the stored expression in ExpressionRewriter."""
 
-    def _inner(self: ExpressionRewriter, *args: P.args, **kwargs: P.kwargs) -> Expression[T]:
+    def _inner(self: ExpressionRewriter, *args: P.args, **kwargs: P.kwargs) -> Expr[T]:
         self.expression = function(self, *args, **kwargs)
         return self.expression
 
@@ -26,8 +23,8 @@ def update_expression(function: Callable[P, Expression[T]]) -> Callable[P, Expre
 class ExpressionRewriter(ABC, Generic[T]):
     """An abstract base class for rewriting expressions."""
 
-    def __init__(self, expression: Expression[T], backend: SymbolicBackend[T]):
-        self._expr: T = backend.as_expression(expression)
+    def __init__(self, expression: Expr[T], backend: SymbolicBackend[T]):
+        self._expr = backend.as_expression(expression)
         self.original_expression = self._expr
         self._backend = backend
 
@@ -37,25 +34,22 @@ class ExpressionRewriter(ABC, Generic[T]):
         return self._expr
 
     @expression.setter
-    def expression(self, other: Expression[T]):
+    def expression(self, other: T):
         self._expr = other
 
     @update_expression
     def evaluate_expression(
         self,
-        variable_assignments: dict[Symbol[T], float],
+        variable_assignments: dict[Expr[T], float],
         original_expression: bool = False,
         functions_map: Mapping[str, Callable[[Any], int | float]] | None = None,
-    ) -> Expression[T]:
+    ) -> T:
         """Assign explicit values to certain variables.
 
         Args:
             variable_assignments : A dictionary of (variable: value) key, val pairs.
             original_expression: Whether or not to evaluate the original expression, by default False.
             functions_map: A map for certain functions.
-
-        Returns:
-            Expression[T]
         """
         return self._backend.substitute(
             self.original_expression if original_expression else self.expression,
@@ -65,27 +59,33 @@ class ExpressionRewriter(ABC, Generic[T]):
 
     @property
     @abstractmethod
-    def variables(self) -> Iterable[Expression[T]]:
+    def variables(self) -> Iterable[T]:
         """Return the variable parameters in the expression."""
 
     @property
     @abstractmethod
-    def as_individual_terms(self) -> Iterable[Expression[T]]:
+    def as_individual_terms(self) -> Iterable[T]:
         """Return the expression as an iterable of individual terms."""
 
     @abstractmethod
     @update_expression
-    def expand(self) -> Expression[T]:
+    def expand(self) -> T:
         """Expand all brackets in the expression."""
 
     @abstractmethod
-    def focus(self, variables: str | Iterable[str]) -> Expression[T]:
+    def focus(self, variables: str | Iterable[str]) -> T:
         """Return an expression containing terms that involve specific variables."""
 
 
 class ResourceRewriter:
+    """A class for rewriting resource expressions in entire routines.
 
-    _expression_rewriter_cls: type[ExpressionRewriter[T]]
+    Args:
+        routine: a CompiledRoutine object with symbolic resources.
+        resource: the resource in the routine we wish to apply rewriting rules to.
+    """
+
+    _rewriter: type[ExpressionRewriter[T]]
 
     def __init__(self, routine: CompiledRoutine, resource: str):
         self.routine = routine
@@ -94,7 +94,7 @@ class ResourceRewriter:
             raise ValueError(f"Routine {routine.name} has no resource {self.resource}.")
         self.top_level_expression = self.routine.resources[self.resource].value
 
-        self.rewriter = self._expression_rewriter_cls(expression=self.top_level_expression)
+        self.rewriter = self._rewriter(expression=self.top_level_expression)
 
     def __getattr__(self, name: str):
         return getattr(self.rewriter, name)
