6824a793419996914726a55af16f8095b1a5a423
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2025-05-29 20:56:48 +0530

    fix(docs): Removed `SymbolicBackend`  from exposing to top-level, and added submodule rendering and extra styling for documentation

--- src/bartiq/symbolics/__init__.py
+++ src/bartiq/symbolics/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .backend import SymbolicBackend
 from .sympy_backend import sympy_backend
 
-__all__ = ["sympy_backend", "SymbolicBackend"]
+__all__ = ["sympy_backend"]
