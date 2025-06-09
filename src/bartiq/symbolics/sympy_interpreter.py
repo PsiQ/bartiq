c01224e68c42f14bc6cc5ad327d150c78f332f09
antalszava <antalszava@gmail.com>
2025-06-09 13:39:09 +0200

    chore: linting

--- src/bartiq/symbolics/sympy_interpreter.py
+++ src/bartiq/symbolics/sympy_interpreter.py
@@ -54,7 +54,17 @@ from sympy import (
     log,
 )
 from sympy import multiplicity as orig_multiplicity
-from sympy import prod, re, sec, sech, sin, sinh, sqrt, tan, tanh
+from sympy import (
+    prod,
+    re,
+    sec,
+    sech,
+    sin,
+    sinh,
+    sqrt,
+    tan,
+    tanh,
+)
 from sympy.codegen.cfunctions import exp2, log2, log10
 from sympy.core.numbers import S as sympy_constants
 from sympy.core.sorting import ordered
