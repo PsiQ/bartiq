f1ebb161afd59456f9d794564835be94f53b3c6c
brendan reid <brendan.reid1991@gmail.com>
2025-06-11 15:55:07 +0100

    Removed fixtures from tests and restructured them

--- tests/analysis/_rewriters/basic_rewriter_tests.py
+++ tests/analysis/_rewriters/basic_rewriter_tests.py
@@ -11,58 +11,68 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from enum import StrEnum
+
 import pytest
 
 from bartiq.analysis._rewriters.expression_rewriter import ExpressionRewriter
 from bartiq.symbolics.backend import SymbolicBackend
 
 
+class CommonExpressions(StrEnum):
+    """Expressions to test rewriters on."""
+
+    TRIVIAL = "a"
+    "Trivial expression; a single symbol."
+
+    SUM_AND_MUL = "a + b + c + d + c*d + a*b"
+    "An expression with sums and multiplications."
+
+    MANY_FUNCS = "a*log2(x/n) + b*(max(0, 1+y, 2+x) + Heaviside(aleph, beth))"
+    "An expression with different non-trivial functions."
+
+    NESTED_MAX = "max(a, 1 - max(b, 1 - max(c, lamda)))"
+    "An expression with nested max functions."
+
+
 class ExpressionRewriterTests:
     rewriter: type[ExpressionRewriter]
     backend: type[SymbolicBackend]
 
-    @pytest.fixture(scope="function")
-    def trivial(self) -> ExpressionRewriter:
-        return self.rewriter("a")
-
-    @pytest.fixture(scope="function")
-    def sum_and_mul(self) -> ExpressionRewriter:
-        return self.rewriter("a + b + c + d + c*d + a*b")
-
-    @pytest.fixture(scope="function")
-    def many_funcs(self) -> ExpressionRewriter:
-        return self.rewriter("a*log2(x/n) + b*(max(0, 1+y, 2+x) + Heaviside(aleph, beth))")
-
-    @pytest.fixture(scope="function")
-    def nested_max(self) -> ExpressionRewriter:
-        return self.rewriter("max(a, 1 - max(b, 1 - max(c, lamda)))")
-
-    def test_trivial(trivial):
-        assert trivial.evaluate_expression({"a": 10}) == 10
+    def assert_expression_seqs_equal(self, actual, expected):
+        assert len(actual) == len(expected) and set(actual) == set(map(self.backend.as_expression, expected))
 
     @pytest.mark.parametrize(
-        "expression, individual_terms",
+        "expression, expected_individual_terms",
         [
-            ["trivial", ["a"]],
-            ["sum_and_mul", ["a", "b", "c", "d", "c*d", "a*b"]],
+            [CommonExpressions.TRIVIAL, ["a"]],
+            [CommonExpressions.SUM_AND_MUL, ["a", "b", "c", "d", "c*d", "a*b"]],
             [
-                "many_funcs",
+                CommonExpressions.MANY_FUNCS,
                 ["a*log2(x/n)", "b*(max(0, 1+y, 2+x) + Heaviside(aleph, beth))"],
             ],
         ],
     )
-    def test_individual_terms(self, expression, individual_terms, request):
-        assert set(request.getfixturevalue(expression).individual_terms) == set(
-            map(self.backend.as_expression, individual_terms)
-        )
+    def test_individual_terms(self, expression, expected_individual_terms):
+        self.assert_expression_seqs_equal(self.rewriter(expression).individual_terms, expected_individual_terms)
 
-    @pytest.mark.parametrize("fixture", ["trivial", "sum_and_mul", "many_funcs", "nested_max"])
-    def test_free_symbols(self, fixture, request):
-        rewriter = request.getfixturevalue(fixture)
-        assert set(rewriter.free_symbols) == set(
-            map(self.backend.as_expression, self.backend.free_symbols(rewriter.expression))
-        )
+    @pytest.mark.parametrize(
+        "expression",
+        [
+            CommonExpressions.TRIVIAL,
+            CommonExpressions.SUM_AND_MUL,
+            CommonExpressions.MANY_FUNCS,
+            CommonExpressions.NESTED_MAX,
+        ],
+    )
+    def test_free_symbols(self, expression):
+        free_symbols_from_rewriter = self.rewriter(expression).free_symbols
+        free_symbols_from_backend = self.backend.free_symbols(self.backend.as_expression(expression))
+        self.assert_expression_seqs_equal(free_symbols_from_rewriter, free_symbols_from_backend)
 
     @pytest.mark.parametrize("focus_on, expected_expression", [["a", "a*(b+1)"], ["c", "c*(d+1)"]])
-    def test_focus(self, focus_on, expected_expression, sum_and_mul):
-        assert sum_and_mul.focus(focus_on) == self.backend.as_expression(expected_expression)
+    def test_focus(self, focus_on, expected_expression):
+
+        assert self.rewriter(CommonExpressions.SUM_AND_MUL).focus(focus_on) == self.backend.as_expression(
+            expected_expression
+        )
