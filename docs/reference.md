7de16fed0e6a88cfc77dd65f50e930e21607a993
brendan reid <brendan.reid1991@gmail.com>
2025-07-24 14:29:29 +0100

    Fixing toc on API reference page; required module docstrings among other things

--- docs/reference.md
+++ docs/reference.md
@@ -1,15 +1,29 @@
-# API reference
+# API Reference
+
+## bartiq
 
 ::: bartiq
 
+## bartiq.analysis
+
 ::: bartiq.analysis
+
+## bartiq.compilation
 
 ::: bartiq.compilation
 
+## bartiq.symbolics
+
 ::: bartiq.symbolics
+
+## bartiq.errors
 
 ::: bartiq.errors
 
+## bartiq.integrations
+
 ::: bartiq.integrations
+
+## bartiq.transform
 
 ::: bartiq.transform
--- src/bartiq/compilation/__init__.py
+++ src/bartiq/compilation/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""The compilation submodule contains routine compilation functionality."""
 from ._compile import CompilationFlags, CompilationResult, compile_routine
 from ._evaluate import EvaluationResult, evaluate
 
