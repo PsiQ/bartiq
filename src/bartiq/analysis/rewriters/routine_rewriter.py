1e9fc9416a12135ad5122d8ee4a1f35fe565f38b
brendan reid <brendan.reid1991@gmail.com>
2025-07-25 12:16:17 +0100

    changed ResourceRewriter to a function

--- src/bartiq/analysis/rewriters/routine_rewriter.py
+++ src/bartiq/analysis/rewriters/routine_rewriter.py
@@ -11,27 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Resource rewriters allow you to rewrite expressions across entire routines."""
+"""Here we provide functionality to allow you to apply rewriters to CompiledRoutine resource expressions"""
 from __future__ import annotations
 
 from collections.abc import Iterable
-from dataclasses import dataclass, replace
-from typing import Generic, Protocol, cast
+from dataclasses import replace
+from typing import Protocol, cast
 
 from bartiq import CompiledRoutine
 from bartiq.analysis.rewriters.expression import ExpressionRewriter
 from bartiq.analysis.rewriters.sympy_expression import sympy_rewriter
-from bartiq.analysis.rewriters.utils import (
-    Assumption,
-    Expand,
-    Initial,
-    Instruction,
-    ReapplyAllAssumptions,
-    Simplify,
-    Substitution,
-)
-from bartiq.symbolics.backend import SymbolicBackend, T
-from bartiq.transform import postorder_transform
+from bartiq.analysis.rewriters.utils import Instruction
+from bartiq.symbolics.backend import T
 
 
 class ExpressionRewriterFactory(Protocol[T]):
@@ -40,174 +31,48 @@ class ExpressionRewriterFactory(Protocol[T]):
     def __call__(self, expression: str | T) -> ExpressionRewriter[T]: ...
 
 
-@dataclass
-class ResourceRewriter(Generic[T]):
-    """A class for rewriting resource expressions of routines.
-
-    It accepts a CompiledRoutine object, a resource name, and an optional rewriter factory function which defaults
-    to `sympy_rewriter`.
-
-    Once instantiated this class creates a `rewriter` attribute using the top level expression of `resource` in the
-    routine. The same methods that can be called on a rewriter can be called on this class.
-
-    When calling methods on an `ExpressionRewriter` class, a new instance is returned. As the `rewriter` is an attribute
-    in this class, calling methods will return a new instance _within this dataclass_. That is, the `rewriter` attribute
-    (and therefore `self`) are updated in-place. Take care when chaining methods together.
-
-    In order to propagate instructions through every level of the routine, the method
-    `apply_to_whole_routine` can be called which will return a new `CompiledRoutine` object,
-    where the relevant resource in every child (at every level) has the current instructions on the rewriter
-    instance applied to its resource expression.
-
-    Note that this _only_ affects `Resources` in the routine. Any substitutions made will not be propagated onto
-    symbolic definitions of Ports, for example.
-
-    Finally, there is a method `from_history` that accepts a list of rewriter `Instructions` and will apply each in turn
-    to the given resource expression.
+def rewrite_routine_resources(
+    routine: CompiledRoutine,
+    resources: str | Iterable[str],
+    instructions: list[Instruction],
+    rewriter_factory: ExpressionRewriterFactory = sympy_rewriter,
+) -> CompiledRoutine:
+    """Rewrite the resources of a CompiledRoutine object with a given list of instructions.
 
     Args:
-        routine: a CompiledRoutine.
-        resource: the resource in the routine we wish to apply rewriting rules to.
-        rewriter_factory: A function that returns rewriter instances, by default `sympy_rewriter`.
-
-    Attributes:
-        rewriter: A rewriter instance applied to the corresponding resource expression of the input routine.
-    """
-
-    routine: CompiledRoutine
-    resource: str
-    rewriter_factory: ExpressionRewriterFactory[T] = sympy_rewriter
-
-    def __post_init__(self):
-        self.rewriter = self.rewriter_factory(self.routine.resources[self.resource].value)
-
-    def __getattr__(self, name: str):
-        """Pipe attribute calls to the expression rewriter instance.
-
-        If the attribute is a method that transforms the rewriter, update our rewriter instance and return `self`
-        to allow method chaining.
-        """
-        attr = getattr(self.rewriter, name)
-
-        if callable(attr):
-
-            def wrapper(*args, **kwargs):
-                """If the method would transform the rewriter attribute, modify it in place and return
-                `self`."""
-                result = attr(*args, **kwargs)
-                if isinstance(result, ExpressionRewriter):
-                    self.rewriter = result
-                    return self
-
-                return result
-
-            return wrapper
-
-        return attr
-
-    def apply_to_whole_routine(self) -> CompiledRoutine:
-        """Apply all instructions currently on the rewriter instance to all children of the routine.
-
-        Returns:
-            A new CompiledRoutine object.
-        """
-
-        @postorder_transform
-        def _traverse_routine(routine: CompiledRoutine, backend: SymbolicBackend) -> CompiledRoutine:
-            """Recursively traverse the routine, replacing resource values
-            starting from the lowest level of children."""
-            if self.resource in routine.resources and not isinstance(
-                routine.resource_values[self.resource], (int | float)
-            ):
-                return replace(
-                    routine,
-                    resources=routine.resources
-                    | {
-                        self.resource: replace(
-                            routine.resources[self.resource],
-                            value=_update_expression(
-                                self.rewriter_factory,
-                                cast(T, routine.resources[self.resource].value),
-                                self.rewriter.history(),
-                            ),
-                        )
-                    },
-                )
-
-            return routine
-
-        return _traverse_routine(self.routine, self.rewriter.backend)
-
-    @classmethod
-    def from_history(
-        cls,
-        routine: CompiledRoutine,
-        resource: str,
-        history: list[Instruction],
-        rewriter_factory: ExpressionRewriterFactory[T] = sympy_rewriter,
-    ) -> ResourceRewriter:
-        """Generate a new ResourceRewriter from a history of instructions.
-
-        Args:
-            routine: A compiled routine object.
-            resource: A resource name.
-            history: A list of rewriter `Instructions` to apply.
-            rewriter_factory: Optional rewriter factory function. Defaults to sympy_rewriter.
-
-        Returns:
-            A ResourceRewriter object, with the history applied to the input resource expression.
-        """
-        resource_rewriter = cls(routine, resource, rewriter_factory)
-        rewriter = resource_rewriter.rewriter
-        for instruction in history:
-            rewriter = _apply_instruction(rewriter=rewriter, instruction=instruction)
-        resource_rewriter.rewriter = rewriter
-        return resource_rewriter
-
-
-def _apply_instruction(rewriter: ExpressionRewriter[T], instruction: Instruction) -> ExpressionRewriter[T]:
-    """Helper function to apply instructions to a rewriter instance.
-
-    Args:
-        rewriter: Rewriter to apply an instruction to.
-        instruction: Instruction to apply.
-
-    Raises:
-        ValueError: If an unrecognised instruction is passed.
+        routine: A CompiledRoutine object.
+        resources: Resource name(s) to apply the instructions to.
+        instructions: A list of rewriter instructions to apply.
+        rewriter_factory: A factory function that instantiates a rewriter. By default sympy_rewriter.
 
     Returns:
-        A new expression rewriter instance.
+        A new CompiledRoutine object.
     """
-    match instruction:
-        case Initial():
-            return rewriter
-        case Expand():
-            return rewriter.expand()
-        case Simplify():
-            return rewriter.simplify()
-        case Assumption():
-            return rewriter.assume(instruction)
-        case Substitution():
-            return rewriter.substitute(instruction.expr, instruction.replacement)
-        case ReapplyAllAssumptions():
-            return rewriter.reapply_all_assumptions()
-        case _:
-            raise ValueError(f"Unrecognised instruction: '{instruction}' has type '{type(instruction)}'.")
 
+    def _traverse_routine(routine: CompiledRoutine, resource: str) -> CompiledRoutine:
+        """Recursively traverse the routine, replacing resource values
+        starting from the lowest level of children."""
+        for child_routine in routine.children.values():
+            return _traverse_routine(child_routine, resource)
+        if resource in routine.resources and not isinstance(routine.resource_values[resource], (int | float)):
+            return replace(
+                routine,
+                resources=routine.resources
+                | {
+                    resource: replace(
+                        routine.resources[resource],
+                        value=rewriter_factory(cast(T, routine.resources[resource].value)).with_instruction_set(
+                            instructions
+                        ),
+                    )
+                },
+            )
 
-def _update_expression(
-    rewriter_factory: ExpressionRewriterFactory[T], expression: T, instructions: Iterable[Instruction]
-) -> T:
-    """Update an expression given a list of rewriting instructions.
+        return routine
 
-    Args:
-        expression: The expression to rewrite.
-        instructions: A list of instructions to apply.
+    if isinstance(resources, str):
+        return _traverse_routine(routine, resources)
 
-    Returns:
-        A new expression with the given modifications.
-    """
-    rewriter = rewriter_factory(expression)
-    for instr in instructions:
-        rewriter = _apply_instruction(rewriter=rewriter, instruction=instr)
-    return rewriter.expression
+    for resource in resources:
+        routine = _traverse_routine(routine, resource)
+    return routine
