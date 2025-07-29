c8b768afccfa4e25bec440de864600f93c9453ec
Konrad Jałowiecki <dexter2206@gmail.com>
2025-07-29 12:19:40 +0200

    fix: Add missing imports to bartiq.analysis

--- src/bartiq/analysis/__init__.py
+++ src/bartiq/analysis/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The bartiq analysis module provides methods to manipulate symbolic expressions."""
+from bartiq.analysis.optimization import BigO, minimize
 from bartiq.analysis.rewriters import ResourceRewriter, sympy_rewriter
 
-__all__ = ["sympy_rewriter", "ResourceRewriter"]
+__all__ = ["BigO", "sympy_rewriter", "ResourceRewriter"]
